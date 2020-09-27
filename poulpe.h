2e99801dbae34d95d8212c2d12fc3905ba34f2fc
ThomasAUB <aubert.thms@gmail.com>
2020-09-27 15:19:33 +0200

    multiple virt implem

--- poulpe.h
+++ poulpe.h
@@ -4,14 +4,7 @@
 
 
 ////////////////////////////////////////////////////////////////////////////////
-/*
-template<typename notifier_t, typename signal_t> 
-struct VirtGen{
-	void sendSignal(signal_t s) override final{
-		notifier_t::sP.sendSignal(s);
-	}
-};
-*/
+
 template<typename...signal_types>
 struct SignalTypes{ using signals = std::tuple<signal_types...>; };
 
@@ -25,6 +18,8 @@ struct NotifierFactory_M : public T {
 	NotifierFactory_M(args_t...a):T(a...){}
 
 private:
+
+	template<size_t FT> struct FakeType{};
 	
 	static constexpr size_t kSignalCount = 
 	std::tuple_size<typename signal_types_t::signals>::value;
@@ -32,59 +27,36 @@ private:
 	template<size_t I>
 	using sig_t = 
 	typename std::tuple_element<I, typename signal_types_t::signals>::type;
+
+	template<size_t I>
+	using sigC_t = 
+	typename std::conditional_t<
+	I < kSignalCount, 
+	sig_t<I%kSignalCount>, FakeType<I> >;
 	
-	void sendSignal(sig_t<0> s) override final {
-		notifier_t::sP.sendSignal(s);
-	}
-
-	void sendSignal(sig_t<1> s) override final {
-		notifier_t::sP.sendSignal(s);
-	}
-
-	void sendSignal(sig_t<2> s) override final {
-		notifier_t::sP.sendSignal(s);
-	}
-
-	/*typename std::enable_if< 3 < kSignalCount, void>
-	sendSignal(std::enable_if< 3 < kSignalCount, sig_t<3>> s){
-		notifier_t::sP.sendSignal(s);
-	}*/
-
-	/*typename std::enable_if<3 < kSignalCount, void>
-	signalSender(){
-		
-	}*/
-
-	/*void sendSignal(sig_t<3> s) override final {
-		notifier_t::sP.sendSignal(s);
-	}*/
-
-	/*void sendSignal(const SEQ_signal& s){
-		notifier_t::sP.sendSignal(s);
-	}*/
-
-	//virtual void sendSignal(sig_t<1>& s){}
-
-	//virtual void sendSignal(sig_t<2> s){}
-
-	//typename std::enable_if_t<0 < kSignalCount, void>
-	//sendSignal(sig_t<0> s) override final {}
-
-	/*typename std::enable_if_t<1 < kSignalCount, void>
-	sendSignal(sig_t<1> s) override final {}*/
+	void sendSignal(sigC_t<0> s) final { notifier_t::sP.sendSignal(s); }
+	void sendSignal(sigC_t<1> s) { notifier_t::sP.sendSignal(s); }
+	void sendSignal(sigC_t<2> s) { notifier_t::sP.sendSignal(s); }
+	void sendSignal(sigC_t<3> s) { notifier_t::sP.sendSignal(s); }
+	void sendSignal(sigC_t<4> s) { notifier_t::sP.sendSignal(s); }
+	void sendSignal(sigC_t<5> s) { notifier_t::sP.sendSignal(s); }
+	void sendSignal(sigC_t<6> s) { notifier_t::sP.sendSignal(s); }
+	void sendSignal(sigC_t<7> s) { notifier_t::sP.sendSignal(s); }
+	void sendSignal(sigC_t<8> s) { notifier_t::sP.sendSignal(s); }
+	void sendSignal(sigC_t<9> s) { notifier_t::sP.sendSignal(s); }
 	
 };
 ////////////////////////////////////////////////////////////////////////////////
 
 
-
+/*
 template<typename notifier_t, typename T, typename signal_t, typename...args_t>
 struct NotifierFactory : public T{
 	NotifierFactory(args_t...a):T(a...){}
-	virtual void sendSignal(signal_t s) final override {
+	void sendSignal(signal_t s) final {
 		notifier_t::sP.sendSignal(s);
 	}
-};
+};*/
 
 
 
