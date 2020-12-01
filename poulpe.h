1571d4ad89c4bed85e3d9fdff9b796e87a1a068e
ThomasAUB <45536491+ThomasAUB@users.noreply.github.com>
2020-12-01 11:02:06 +0100

    Update poulpe.h
    updates names

--- poulpe.h
+++ poulpe.h
@@ -5,17 +5,17 @@
 
 
 #define DEFINE_LISTENERS(T...)                                                  \
-struct Notifier{                                                                \
+struct Emitter{                                                                \
 	using poulpe_t = Poulpe<T>;						\
 	template<typename signal_t>                                             \
-	void sendSignal(signal_t& s){ sP.sendSignal(s); }                       \
+	void pEmit(signal_t& s){ sP.pEmit(s); }                       \
 	static poulpe_t& sP;                                                    \
 };										\
 
 
 #define CREATE_POULPE(p...)                                                     \
-Notifier::poulpe_t	gPoulpe(p);              		                \
-Notifier::poulpe_t&	Notifier::sP = gPoulpe;                   		\
+Emitter::poulpe_t	gPoulpe(p);              		                \
+Emitter::poulpe_t&	Emitter::sP = gPoulpe;                   		\
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -24,20 +24,20 @@ template<typename...signal_types>
 struct SignalTypes{ using signals = std::tuple<signal_types...>; };
 
 template<
-typename notifier_t,
+typename emitter_t,
 typename T,
 typename signal_types_t,
 typename...args_t>
-struct NotifierFactory final : public T {
+struct EmitterFactory final : public T {
 
-    NotifierFactory(args_t...a):T(a...){}
+    EmitterFactory(args_t...a):T(a...){}
 
 private:
 
     static constexpr size_t kSignalCount =
     std::tuple_size<typename signal_types_t::signals>::value;
 
-    template<size_t FT> struct FakeType{};
+    template<size_t I> struct DummyType{};
 
     template<size_t I>
     using sig_t =
@@ -47,9 +47,9 @@ private:
     using cond_sig_t =
     typename std::conditional_t<
     I < kSignalCount,
-    sig_t<I%kSignalCount>, FakeType<I> >;
+    sig_t<I%kSignalCount>, DummyType<I> >;
 	
-#define REPEAT_1	void sendSignal(cond_sig_t<__COUNTER__>& s) { notifier_t::sP.sendSignal(s); }
+#define REPEAT_1	void pEmit(cond_sig_t<__COUNTER__>& s) { emitter_t::sP.pEmit(s); }
 #define REPEAT_2	REPEAT_1	REPEAT_1
 #define REPEAT_4	REPEAT_2	REPEAT_2
 #define REPEAT_8	REPEAT_4	REPEAT_4
@@ -73,32 +73,32 @@ template<typename...T>
 struct Poulpe{
 
     constexpr Poulpe(T&...p) :
-    mListeners(std::tuple<T&...>(p...))
+    mReceivers(std::tuple<T&...>(p...))
     {}
 
     template<typename signal_t>
-    void sendSignal(signal_t& s){
+    void pEmit(signal_t& s){
         static_assert(!std::is_fundamental<signal_t>::value,
-            "Invalid signal type : can't be fundamental type");
+            "Invalid signal type : can't be fundamental type");        
         process_caller(s);
     }
 
 private:
 
-    using listeners_t = std::tuple<T&...>;
+    using receivers_t = std::tuple<T&...>;
 
-    static constexpr size_t kListSize = std::tuple_size<listeners_t>::value;
+    static constexpr size_t kListSize = std::tuple_size<receivers_t>::value;
 
-    const listeners_t mListeners;
+    const receivers_t mReceivers;
 
     template <size_t I = 0, typename signal_t>
     typename std::enable_if<I == kListSize, void>::type
-    process_caller(signal_t& s){}
+    process_caller(signal_t& s){ static_cast<void>(s); }
 
     template <size_t I = 0, typename signal_t>
     typename std::enable_if<(I < kListSize), void>::type
     process_caller(signal_t& s){
-        std::get<I>(mListeners).receiveSignal(s);
+        std::get<I>(mReceivers).pReceive(s);
         process_caller<I+1>(s);
     }
 
