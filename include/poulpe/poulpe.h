8c53161172bba52abca87bdb37a991054411ac91
Thomas Aubert <thomas.aubert@arturia.com>
2021-06-25 10:50:45 +0200

    define IsReceiver helper inside Poulpe class

--- include/poulpe/poulpe.h
+++ include/poulpe/poulpe.h
@@ -51,40 +51,6 @@ Emitter::poulpe_t&	Emitter::sP = gPoulpe;                                      	
 
 
 
-// Primary template with a static assertion
-// for a meaningful error message
-// if it ever gets instantiated.
-// We could leave it undefined if we didn't care.
-
-template<typename, typename T>
-struct IsReceiver {
-    static_assert(
-        std::integral_constant<T, false>::value,
-        "Second template parameter needs to be of function type.");
-};
-
-// specialization that does the checking
-
-template<typename C, typename Ret, typename... Args>
-struct IsReceiver<C, Ret(Args...)> {
-private:
-    template<typename T>
-    static constexpr auto check(T*)
-    -> typename
-        std::is_same<
-            decltype( std::declval<T>().pReceive( std::declval<Args>()... ) ),
-            Ret    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        >::type;  // attempt to call it and see if the return type is correct
-
-    template<typename>
-    static constexpr std::false_type check(...);
-
-    typedef decltype(check<C>(0)) type;
-
-public:
-    static constexpr bool value = type::value;
-};
-
 
 
 
@@ -113,6 +79,40 @@ private:
     static constexpr size_t kListSize = std::tuple_size<receivers_t>::value;
 
     const receivers_t mReceivers;
+
+    // Primary template with a static assertion
+    // for a meaningful error message
+    // if it ever gets instantiated.
+    // We could leave it undefined if we didn't care.
+
+    template<typename, typename T>
+    struct IsReceiver {
+        static_assert(
+            std::integral_constant<T, false>::value,
+            "Second template parameter needs to be of function type.");
+    };
+
+    // specialization that does the checking
+
+    template<typename C, typename Ret, typename... Args>
+    struct IsReceiver<C, Ret(Args...)> {
+    private:
+        template<typename T>
+        static constexpr auto check(T*)
+        -> typename
+            std::is_same<
+                decltype( std::declval<T>().pReceive( std::declval<Args>()... ) ),
+                Ret    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+            >::type;  // attempt to call it and see if the return type is correct
+
+        template<typename>
+        static constexpr std::false_type check(...);
+
+        typedef decltype(check<C>(0)) type;
+
+    public:
+        static constexpr bool value = type::value;
+    };
 
     // last call
     template<size_t I = 0, typename signal_t>
